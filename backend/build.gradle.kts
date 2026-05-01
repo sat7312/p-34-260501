7971809ac88fb0f0a5740624e05d0f99fdde0ff3
sat7312 <sat7312@gmail.com>
2026-05-01 14:18:48 +0900

    Member, Post, lombok 제거

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	kotlin("plugin.spring") version "2.2.21"
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "2.2.21"
 }
 
 group = "com.back"
