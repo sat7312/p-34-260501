138face8a0af937733419377bb35bc5e7cef21d8
sat7312 <sat7312@gmail.com>
2026-05-04 11:40:43 +0900

    32 PostService 변환

---
+++ backend/src/main/java/com/back/domain/post/post/service/PostService.kt
@@ -0,0 +1,41 @@
+package com.back.domain.post.post.service
+
+import com.back.domain.member.entity.Member
+import com.back.domain.post.post.entity.Post
+import com.back.domain.post.post.repository.PostRepository
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@RequiredArgsConstructor
+class PostService(private val postRepository: PostRepository) {
+
+    @Transactional
+    fun write(author: Member, title: String, content: String): Post {
+        val post = Post(author, title, content)
+        return postRepository.save<Post>(post)
+    }
+
+    fun modify(id: Int, title: String, content: String): Post =
+        postRepository.findById(id).get().apply {
+            update(title, content)
+        }
+
+    fun deleteById(id: Int) =
+        postRepository.deleteById(id)
+
+    fun findById(id: Int): Optional<Post> =
+        postRepository.findById(id)
+
+    fun count(): Long =
+        postRepository.count()
+
+    fun findAll(): List<Post> =
+        postRepository.findAll()
+
+    fun flush() =
+        postRepository.flush()
+}
+
